5ef6fe1092b73fe37dbb5b5e6713e40ece03e83b
Athov07 <atharvadsare17@gmail.com>
2026-03-06 11:06:34 +0530

    imcomplete file push

--- CommentService/models/comment.model.js
+++ CommentService/models/comment.model.js
@@ -17,7 +17,7 @@ const commentSchema = new mongoose.Schema(
       required: true,
     },
 
-    userAvatar: {
+    profileImage: {
       type: String,
       default: "",
     },
@@ -33,7 +33,7 @@ const commentSchema = new mongoose.Schema(
       },
     ],
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 export default mongoose.model("Comment", commentSchema);
